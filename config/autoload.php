39a2c9c4ea94e7d4c551eadfb495ed87f9ccd7b7
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-03-12 10:31:11 +0100

    Added autoload and htaccess files for Contao 3

---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * conditionalformfields extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-conditionalformfields
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    'ConditionalFormFields' => 'system/modules/conditionalformfields/ConditionalFormFields.php',
+));
