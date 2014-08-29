bc7a45afb7a3805af0782459290b8f6944405ee1
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-08-29 13:34:45 +0200

    Fixed form fields without expert legend not displaying the conditional form field checkbox (#4)

--- ConditionalFormFields.php
+++ ConditionalFormFields.php
@@ -32,7 +32,18 @@ class ConditionalFormFields extends Controller
                 continue;
             }
 
-            $GLOBALS['TL_DCA']['tl_form_field']['palettes'][$k] = preg_replace('/({expert_legend(:hide)?})/u', '$1,isConditionalFormField', $GLOBALS['TL_DCA']['tl_form_field']['palettes'][$k]);
+            if (strpos('{expert_legend', $GLOBALS['TL_DCA']['tl_form_field']['palettes'][$k]) !== false) {
+                $GLOBALS['TL_DCA']['tl_form_field']['palettes'][$k] = preg_replace(
+                    '/({expert_legend(:hide)?})/u',
+                    '$1,isConditionalFormField',
+                    $GLOBALS['TL_DCA']['tl_form_field']['palettes'][$k]
+                );
+            } else {
+                $GLOBALS['TL_DCA']['tl_form_field']['palettes'][$k] = rtrim(
+                    $GLOBALS['TL_DCA']['tl_form_field']['palettes'][$k],
+                    ';'
+                ) . ';{expert_legend:hide},isConditionalFormField';
+            }
         }
 
     }
