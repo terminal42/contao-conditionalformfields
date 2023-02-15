72132a70dd699a8c2c6dd5e87f8428efa8143e4b
Yanick Witschi <yanick.witschi@terminal42.ch>
2023-02-15 14:53:05 +0100

    Fixed possible invalid array access

--- src/FormHandler.php
+++ src/FormHandler.php
@@ -58,7 +58,7 @@ class FormHandler
         if (!empty($this->conditions)) {
             // Add CSS class for current form
             $formAttributes = StringUtil::deserialize($form->attributes, true);
-            $formAttributes[1] = trim($formAttributes[1].' cff');
+            $formAttributes[1] = trim(($formAttributes[1] ?? '').' cff');
 
             if (!empty(($previousData = $this->getPreviousDataFromMpForms()))) {
                 $formAttributes[1] .= '" data-cff-previous="'.StringUtil::specialcharsAttribute(json_encode($previousData, JSON_THROW_ON_ERROR));
