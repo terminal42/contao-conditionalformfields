1d8240fa32e3b862d2ec62481a563181f57339e6
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-03-27 09:15:01 +0100

    CS

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -7,5 +7,9 @@ return (new Configuration())
     // Optional integrations
     ->ignoreErrorsOnPackage('terminal42/contao-mp_forms', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 
+    // Only required for PHP < 8.4
+    ->ignoreErrorsOnPackage('symfony/polyfill-php84', [ErrorType::UNUSED_DEPENDENCY])
+
+    // class in DCA file
     ->ignoreUnknownClasses([\tl_form_field::class])
 ;
